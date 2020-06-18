e2f2695a1fcc74d407ba58260b17d79e5f439883
irgret <irgret@gmail.com>
2020-06-18 23:05:39 +0300

    сделана основная часть, дополнительная в процессе

--- homework 7.sql
+++ homework 7.sql
@@ -1,1 +1,19 @@
--- приступила к выполнению, пока все очень тяжело дается
+/* 1. Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине. */
+
+SELECT order_id FROM orders_products WHERE total > 0; -- узнаем id товара, который был когда-либо куплен
+
+SELECT user_id FROM orders WHERE id IN (SELECT order_id FROM orders_products WHERE total > 0); -- узнаем, кто купил
+
+-- находим покупателей по именам
+SELECT name FROM users WHERE id IN (SELECT user_id FROM orders WHERE id IN (SELECT order_id FROM orders_products WHERE total > 0));
+-- пыталась сделать результирующую таблицу, выводящую соответствие покупателя с купленным им товаром, но не вышло
+
+
+/* 2. Выведите список товаров products и разделов catalogs, который соответствует товару.*/
+
+SELECT products.name as 'название', catalogs.name as 'тип' FROM products, catalogs WHERE 
+	products.catalog_id = catalogs.id;
+
+
+
+
